1439770c9857c129a9ad6d54eb3834feca4e596b
Edoardo Baravaglio <edo.baravaglio@gmail.com>
2026-02-11 01:54:19 +0100

    release: v1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.redergo.buspullman"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10002
-        versionName = "1.0.2"
+        versionCode = 10003
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
